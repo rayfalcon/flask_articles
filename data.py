b717073d11466aa1bfbb86f78e5ff013b0cb9c89
Cory Maloney <cmaloney83@gmail.com>
2021-04-01 23:13:57 -0400

    Initial Commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article 1',
+            'body': 'Hello There peoples',
+            'author': 'John Doe',
+            'create_date': '04-25-2021'
+        },
+        {
+            'id': 2,
+            'title': 'Article 2',
+            'body': 'Hello There peoples',
+            'author': 'Jane Doe',
+            'create_date': '04-25-2021'
+        },
+        {
+            'id': 3,
+            'title': 'Article 3',
+            'body': 'Hello There peoples',
+            'author': 'Jack Doe',
+            'create_date': '04-25-2021'
+        }
+    ]
+    return articles
